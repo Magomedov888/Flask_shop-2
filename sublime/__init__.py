44d3d3cb726983ed793ecfa47619fc7e22f9c906
Magomedov888 <m.abdulla05@gmail.com>
2022-04-19 05:38:27 +0300

    commit

--- sublime/__init__.py
+++ sublime/__init__.py
@@ -1,5 +1,11 @@
-from flask import Flask 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config.from_objects(Config)
+migrate = Migrate(app, db)
 
 from sublime import routes 
---
+++ sublime/models.py
@@ -0,0 +1,16 @@
+from sublime import db
+
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(), nullable=False) 
+    price = db.Column(db.Integer(), nullable=False)
+    category = db.Column(db.String(), nullable=False) 
+    availibility = db.Column(db.String(), nullable=False) 
+    description = db.Column(db.Text(), nullable=False) 
+    image = db.Column(db.String(), nullable=False) 
+
+    def __repr__(self) -> str:
+        return self.title
